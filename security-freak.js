4917f632bdbfc3f597fce2a2fb1a8ea3f9fdb140
Kishan Patel <coolsandyman@gmail.com>
2014-12-30 22:51:31 +0530

    Comments added to the html and javascript file

--- security-freak.js
+++ security-freak.js
@@ -4,17 +4,19 @@ document.onmousedown = disableRtClick;
 
 document.onkeyup=disablePrtScr;
 
+//Right click disabled
 function disableRtClick(event){
 	if(event.button==2){
 		alert("Sorry..right click disabled..");
 		return false;
-			
 	}
 }
 
+//Print Screen disabled
 function disablePrtScr(e){
 	if(e.which==44){
 		alert("Sorry..Print Screen Is Blocked.");
+		return false;
 	}
 }
 
